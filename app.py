d398c2cdf6553c22882061992cecfaf83998d004
Toby Waite <toby.waite+github@gmail.com>
2014-10-17 14:56:58 -0700

    basic flask app to serve static content

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 33507))
+
+    print('using port %s' % port)
+
+    app.run(host='0.0.0.0', port=port)
